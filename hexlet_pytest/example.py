b59c9171719824f3efc37d3168aec9b74331f289
family <family-laptop@MacBook-Air-family.local>
2023-07-11 15:28:35 +0200

    step two - test for LIFO

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,3 +1,17 @@
 # Эта функция переворачивает переданную строку
 def reverse(string):
     return string[::-1]
+
+# Стэк
+stack = []
+not stack
+stack.append(1)
+stack.append(2)
+stack.append(3)
+not stack
+stack
+stack.pop()
+stack.pop()
+stack.pop()
+not stack
+
